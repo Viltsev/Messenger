31e9f85e1b709aa202c14e4161d260470f5aa29d
Viltsev <94001621+Viltsev@users.noreply.github.com>
2024-02-05 19:10:16 +0400

    basics implementation of real chat with websockets

---
+++ EnglishMessenger/src/main/java/com/engmes/EnglishMessenger/EnglishMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.engmes.EnglishMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishMessengerApplication.class, args);
+	}
+
+}
