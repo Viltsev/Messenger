bb762c8edcd72ed89b2a24dbff591bcf93fbc68f
AnastasyaMigunova <65sar.ru@gmail.com>
2024-02-25 20:18:17 +0400

    add dialog

---
+++ EnglishMessenger/src/main/java/com/engmes/EnglishMessenger/Dialog/controllers/DialogController.java
@@ -0,0 +1,25 @@
+package com.engmes.EnglishMessenger.Dialog.controllers;
+
+
+import com.engmes.EnglishMessenger.Dialog.services.DialogService;
+import com.engmes.EnglishMessenger.Profile.model.User;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dialog")
+@AllArgsConstructor
+public class DialogController {
+    private static final Logger logger = LoggerFactory.getLogger(DialogController.class);
+
+    @Autowired
+    private final DialogService dialogService;
+
+    @PostMapping("/generateDialog")
+    public User generateDialog(@RequestBody String email) {
+        return dialogService.generateDialog(email);
+    }
+}
