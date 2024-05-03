7b07e278b3498346585ef0f55361ab9c06dc189a
AnastasyaMigunova <65sar.ru@gmail.com>
2024-05-03 13:53:39 +0400

    fixing dictionary

--- EnglishMessenger/src/main/java/com/engmes/EnglishMessenger/Dictionary/repo/DictionaryRepository.java
+++ EnglishMessenger/src/main/java/com/engmes/EnglishMessenger/Dictionary/repo/DictionaryRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface DictionaryRepository extends JpaRepository<Word, Long> {
-    @Query("SELECT w FROM Word w WHERE w.word LIKE %?1%")
+    @Query("SELECT w FROM Word w WHERE w.word = ?1")
     Word findWord(String searchWord);
 }
